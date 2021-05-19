6b7da35b4aa5a3d9ebde766ae27c7f5d549bb585
RoseBud13 <xiongkaijie13@gmail.com>
2021-05-19 13:07:27 +0800

    frontend reset and flask cors added

--- server/rasapi/api.py
+++ server/rasapi/api.py
@@ -5,6 +5,7 @@ api.py
 """
 
 from flask import Blueprint, jsonify, request
+from .models import db, Resource
 
 
 api = Blueprint('api', __name__)
@@ -14,3 +15,23 @@ api = Blueprint('api', __name__)
 def say_hello(name):
     response = { 'msg': "Hello {}".format(name) }
     return jsonify(response)
+
+
+@api.route('/resources/', methods=('GET','POST'))
+def get_add_resources():
+    if request.method == 'GET':
+        resources = Resource.query.all()
+        return jsonify({'resources': [r.to_dict() for r in resources]})
+    elif request.method == 'POST':
+        data = request.get_json()
+        resource = Resource(rsrc_name=data['rsrc_name'], status=data['status'], description=data['description'], rsrc_type=data['rsrc_type'])
+        db.session.add(resource)
+        db.session.commit()
+        return jsonify(resource.to_dict()), 201
+
+
+@api.route('/resources/<int:id>/')
+def get_resource(id):
+    resource = Resource.query.get(id)
+    return jsonify({'resource': resource.to_dict()})
+
--- server/rasapi/application.py
+++ server/rasapi/application.py
@@ -4,11 +4,14 @@ application.py
 """
 
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(app_name='RAS_API'):
     app = Flask(app_name)
     app.config.from_object('rasapi.config.BaseConfig')
+
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     from rasapi.api import api
     app.register_blueprint(api, url_prefix="/")
