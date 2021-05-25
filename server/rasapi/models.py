29bf0945a63f504fc1183e71c491f7f2217e8c5a
RoseBud13 <xiongkaijie13@gmail.com>
2021-05-25 13:55:04 +0800

    models update

--- server/rasapi/models.py
+++ server/rasapi/models.py
@@ -18,6 +18,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     username = db.Column(db.String(50))
     user_type = db.Column(db.String(20))
+    resources = db.relationship('Resource', backref="user", lazy=False)
 
     def to_dict(self):
         return dict(id=self.id,
@@ -25,7 +26,8 @@ class User(db.Model):
                     cdsid=self.cdsid,
                     password_hash=self.password_hash,
                     username=self.username,
-                    user_type=self.user_type)
+                    user_type=self.user_type,
+                    resources=[resource.to_dict() for resource in self.resources])
 
 
 class Resource(db.Model):
@@ -33,13 +35,21 @@ class Resource(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     rsrc_name = db.Column(db.String(50))
+    department = db.Column(db.String(50))
     status = db.Column(db.String(20))
+    ci_level = db.Cloumn(db.String(20))
+    responsible_user = db.Column(db.Integer, db.ForeignKey('resources.id'))
+    location = db.Column(db.String(255))
     description = db.Column(db.String(255))
     rsrc_type = db.Column(db.String(20))
 
     def to_dict(self):
         return dict(id=self.id,
                     rsrc_name=self.rsrc_name,
+                    department=self.department,
                     status=self.status,
+                    ci_level=self.ci_level,
+                    responsible_user=self.responsible_user,
+                    location=self.location,
                     description=self.description,
                     rsrc_type=self.rsrc_type)
