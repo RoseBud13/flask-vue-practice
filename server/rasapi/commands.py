e0c141d002841fa4970b50ad34543d5b3d0c9f03
RoseBud13 <xiongkaijie13@gmail.com>
2021-05-25 16:59:32 +0800

    models and commands update

--- server/rasapi/commands.py
+++ server/rasapi/commands.py
@@ -29,15 +29,15 @@ def forgersrc():
     db.create_all()
 
     resources = [
-        {'rsrc_name': 'ad-adlab-hil-1', 'status': 'active', 'description': 'AD Lab Vector HIL rig for VCU1 and AD sensors', 'rsrc_type': 'HIL'},
-        {'rsrc_name': 'adas-component-hil', 'status': 'active', 'description': 'asdm flc2 flr production rig', 'rsrc_type': 'HIL'},
-        {'rsrc_name': 'eh-icup-1', 'status': 'active', 'description': 'used for EH 522A and 622A project test', 'rsrc_type': 'Boxcar'},
-        {'rsrc_name': 'adas-domian-hil-argus2-pds-1', 'status': 'removed', 'description': 'PDS ADAS HIL rig', 'rsrc_type': 'HIL'},
-        {'rsrc_name': 'steer-component-pscm-cma-pds-2', 'status': 'maintenance', 'description': 'PDS steer bench', 'rsrc_type': 'Test bench'},
+        {'rsrc_name': 'ad-adlab-hil-1', 'department': 'dept', 'status': 'active', 'ci_level': 'component', 'responsible_user': '1', 'location': 'CIC', 'description': 'AD Lab Vector HIL rig for VCU1 and AD sensors', 'rsrc_type': 'HIL'},
+        {'rsrc_name': 'adas-component-hil', 'department': 'dept', 'status': 'active', 'ci_level': 'component', 'responsible_user': '1', 'location': 'CIC', 'description': 'asdm flc2 flr production rig', 'rsrc_type': 'HIL'},
+        {'rsrc_name': 'eh-icup-1', 'department': 'dept', 'status': 'active', 'ci_level': 'component', 'responsible_user': '1', 'location': 'CIC', 'description': 'used for EH 522A and 622A project test', 'rsrc_type': 'Boxcar'},
+        {'rsrc_name': 'adas-domian-hil-argus2-pds-1', 'department': 'dept', 'status': 'removed', 'ci_level': 'component', 'responsible_user': '1', 'location': 'CIC', 'description': 'PDS ADAS HIL rig', 'rsrc_type': 'HIL'},
+        {'rsrc_name': 'steer-component-pscm-cma-pds-2', 'department': 'dept', 'status': 'maintenance', 'ci_level': 'component', 'responsible_user': '1', 'location': 'CIC', 'description': 'PDS steer bench', 'rsrc_type': 'Test bench'},
     ]
 
     for r in resources:
-        resource = Resource(rsrc_name=r['rsrc_name'], status=r['status'], description=r['description'], rsrc_type=r['rsrc_type'])
+        resource = Resource(rsrc_name=r['rsrc_name'], department=r['department'], status=r['status'], ci_level=r['ci_level'], responsible_user=r['responsible_user'], location=r['location'], description=r['description'], rsrc_type=r['rsrc_type'])
         db.session.add(resource)
 
     db.session.commit()
